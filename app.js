be695ecbe1449a75b38f081bd491eacb24bf08a3
ignaciogudino <ignaciogudino1@hotmail.com>
2023-01-25 22:45:14 -0300

    Evaluacion Elemental

---
+++ app.js
@@ -0,0 +1,7 @@
+document.getElementById("buttonPopUp").addEventListener("click", function(){
+    document.querySelector(".container-popup").style.display = "block";
+})
+
+document.getElementById("closePopUp").addEventListener("click", function(){
+    document.querySelector(".container-popup").style.display = "none";
+})
