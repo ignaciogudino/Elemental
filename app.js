f0e15b8a4b7e36138acab9f50af4c05500b344c3
ignaciogudino <ignaciogudino1@hotmail.com>
2023-01-27 12:55:41 -0300

    Se agregan nuevos modulos y funcionalidades

--- app.js
+++ app.js
@@ -2,6 +2,31 @@ document.getElementById("buttonPopUp").addEventListener("click", function(){
     document.querySelector(".container-popup").style.display = "block";
 })
 
+document.getElementById("navPopUp").addEventListener("click", function(){
+    document.querySelector(".container-popup").style.display = "block";
+})
+
 document.getElementById("closePopUp").addEventListener("click", function(){
     document.querySelector(".container-popup").style.display = "none";
 })
+
+let infoDesplegada = false;
+
+document.getElementById("arrowOpenInfo").addEventListener("click", function(){
+        infoDesplegada = !infoDesplegada
+        if(infoDesplegada){
+            document.querySelector(".slot-text").style.display = "block"
+            document.getElementById("arrowOpenInfo").style.transform = "rotate(180deg)"
+            document.getElementById("arrowOpenInfo").src = "images/up-arrow.png"
+        }else{
+            document.querySelector(".slot-text").style.display = "none"
+            document.getElementById("arrowOpenInfo").style.transform = "rotate(0deg)"
+            document.getElementById("arrowOpenInfo").src = "images/down-arrow.png"
+
+        }
+        // infoDesplegada ? document.querySelector(".slot-text").style.display = "block" : document.querySelector(".slot-text").style.display = "none"
+        // infoDesplegada ? document.getElementById("arrowOpenInfo").style.transform = "rotate(180deg)" : document.getElementById("arrowOpenInfo").style.transform = "rotate(0deg)"
+        // infoDesplegada ? document.getElementById("arrowOpenInfo").src = "images/up-arrow.png" : document.getElementById("arrowOpenInfo").src = "images/down-arrow.png" 
+})
+
+
